30289d7f5660a89a3b54719bea29a19507687b14
Jill Pelavin <jillrenee42@gmail.com>
2024-09-03 22:29:35 -0400

    recursively shows properties now on new lines; dealing with arrays

--- src/ItemDetail.tsx
+++ src/ItemDetail.tsx
@@ -11,24 +11,38 @@ interface DetailProps {
  const ItemDetail: React.FC<DetailProps> = (item) => {
 
     console.log("in item detail:", item);
-     const processValue = (value: any) => {
-         if (typeof value === 'object' && value !== null) {
-             // If the value is an object, recursively render its key-value pairs
-             return (
-                 <div className="nestedObject">
-                     {Object.entries(value).map(([key, innerValue]) => (
-                         <div key={key} className="propertyLine">
-                             <div>{key}:</div>
-                             <div>{processValue(innerValue)}</div>
-                         </div>
-                     ))}
-                 </div>
-             );
-         } else {
-             // If the value is not an object, just render it
-             return <span>{value}</span>;
-         }
-     };
+
+
+         const processValue = (value: any) => {
+             if (Array.isArray(value)) {
+                 // If the value is an array, render it as [val1, val2, ...]
+                 return (
+                     <span>
+          [{value.map((element, index) => (
+                         <span key={index}>
+              {index > 0 && ', '}
+                             {typeof element === 'object' ? JSON.stringify(element) : element}
+            </span>
+                     ))}]
+        </span>
+                 );
+             } else if (typeof value === 'object' && value !== null) {
+                 // If the value is an object, recursively render its key-value pairs
+                 return (
+                     <div className="nestedObject">
+                         {Object.entries(value).map(([key, innerValue]) => (
+                             <div key={key} className="propertyLine">
+                                 <div>{key}:</div>
+                                 <div>{processValue(innerValue)}</div>
+                             </div>
+                         ))}
+                     </div>
+                 );
+             } else {
+                 // If the value is neither an array nor an object, just render it
+                 return <span>{value}</span>;
+             }
+         };
 
     return (<div className="detailLine">
         {Object.entries(item).map(([key, value]) => (
